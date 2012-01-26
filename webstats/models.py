7c2d0c1b83902c4b3b67a058cc7967ae6aa0b29c
quarkonium <dhatton@gmx.ch>
2012-01-26 18:11:52 +0100

    webstats, track

--- webstats/models.py
+++ webstats/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Visitor(models.Model):
+  page = models.CharField(blank=True, max_length=100)
+  ip = models.IPAddressField(blank=True)
+  time = models.DateTimeField(auto_now_add=True, editable=False)
+  referer = models.URLField(blank=True, verify_exists=False)
+  user_agent = models.CharField(blank=True, max_length=100)
--- webstats/views.py
+++ webstats/views.py
@@ -2,7 +2,11 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.template import Context, loader
+from django_webstats.webstats.models import Visitor
 from datetime import datetime
+from django.template import RequestContext
+
 
 @login_required
 def webstats_main_page(request):
@@ -10,10 +14,26 @@ def webstats_main_page(request):
   If users are authenticated, direct them to the main page. Otherwise,
   take them to the login page.
   """
-  return render_to_response('webstats/index.html')
+
+  visitor_list = Visitor.objects.all()
+  #t = loader.get_template('webstats/index.html')
+  c = Context({
+    'visitor_list': visitor_list,
+  })
+  #return render_to_response(t.render(c))
+  return render_to_response('webstats/index.html',
+                            c,
+                            context_instance=RequestContext(request))
 
 def webstats_track(request):
   print request
+  v = Visitor()
+  #v.ip = request.META.get("HTTP_X_FORWARDED_FOR", "") #get visitor ip
+  v.ip = request.META.get("REMOTE_ADDR", "") #get visitor ip
+  v.time = datetime.now() # get visitor time
+  v.referer = request.META.get("HTTP_REFERER", "") #get visitor referer
+  v.user_agent = request.META.get("HTTP_USER_AGENT", "") # get visitor user agent
+  v.save() 
 
   TRANSPARENT_1_PIXEL_GIF = "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b"
   return HttpResponse(TRANSPARENT_1_PIXEL_GIF, content_type = "image/gif")
